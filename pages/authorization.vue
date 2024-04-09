1e9851fb6338d2fc4841cd2f5b6f9a347476d12c
RihardXXX <yusupa.akaeva@yandex.ru>
2024-04-09 18:05:31 +0300

    feat(authorization): otpPassword send

--- pages/authorization.vue
+++ pages/authorization.vue
@@ -34,19 +34,29 @@
     <h1 class="text-center text-6xl pt-4">
       Ввод одноразового пароля
     </h1>
-    <p class="mt-10 text-3xl text-center">
+    <p class="mt-10 mb-20 text-3xl text-center">
       {{ successMessage }}
     </p>
 
-    <UButton
-      type="submit"
-      size="xl"
-      :loading="isLoading"
-      block
-      class="p-8 text-3xl !bg-slate-800 !text-slate-50 mt-8"
-    >
-      вернутся на главную страницу
-    </UButton>
+    <UForm :schema="schemaOtp" :state="stateOtp" class="space-y-4" @submit="onSubmitOtp">
+      <UFormGroup label="одноразовый пароль с почты" name="otpPassword" :class="$style.uform">
+        <UInput
+          v-model="stateOtp.otpPassword"
+          :class="$style.field"
+          placeholder="одноразовый пароль с почты"
+        />
+      </UFormGroup>
+
+      <UButton
+        type="submit"
+        size="xl"
+        :loading="isLoading"
+        block
+        class="p-10 text-5xl !bg-slate-800 !text-slate-50 !mt-10"
+      >
+        отправить
+      </UButton>
+    </UForm>
   </UContainer>
 
   <UContainer v-if="isLast" class="pb-20 h-full font-semibold bg-slate-50 bg-slate-50">
@@ -76,7 +86,7 @@ const toast = useToast()
 
 const textRequired = 'поле является обязательным'
 
-// схема валидации в библиотеке
+// схема валидации в библиотеке при авторизации
 const schema = object({
   email: string().trim().email('неверный формат почты').required(textRequired),
   password: string().min(8, 'пароль должен состоять из более 8 символов').required(textRequired)
@@ -87,6 +97,18 @@ type Schema = InferType<typeof schema>
 const state = reactive({
   email: undefined,
   password: undefined
+})
+
+// ======
+// схема валидации в библиотеке при otp пароле
+const schemaOtp = object({
+  otpPassword: string().trim().required(textRequired)
+})
+
+type SchemaOtp = InferType<typeof schemaOtp>
+
+const stateOtp = reactive({
+  otpPassword: undefined
 })
 
 const isLoading = ref(false)
@@ -131,6 +153,40 @@ async function onSubmitLogin (event: FormSubmitEvent<Schema>) {
     console.log('res: ', res)
     // для показа уведомления
     successMessage.value = res.message
+    step.value = 2
+  } catch (e) {
+    toast.add({
+      title: 'Внимание',
+      description: (e as IErrorResponse).data.message,
+      icon: 'i-octicon-desktop-download-24',
+      timeout: 5000
+    })
+  } finally {
+    isLoading.value = false
+    setDisabledTab(false)
+  }
+}
+
+async function onSubmitOtp (event: FormSubmitEvent<SchemaOtp>) {
+  const data = toRaw(event.data)
+
+  const url = authUrls.value.verify
+  isLoading.value = true
+  setDisabledTab(true)
+
+  try {
+    const res = await $fetch<{ message: string }>(url, {
+      method: 'POST',
+      body: {
+        email: state.email,
+        otp: data.otpPassword
+      }
+    })
+
+    console.log('res: ', res)
+    // для показа уведомления
+    successMessage.value = res.message
+    step.value = 3
   } catch (e) {
     toast.add({
       title: 'Внимание',
